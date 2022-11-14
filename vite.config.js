c967064fed94893c73a868c4c09ad2648c427784
Eric Li <ericyoungli@gmail.com>
2022-11-13 22:37:25 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'vite-test',
+  base: 'vite-template',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
